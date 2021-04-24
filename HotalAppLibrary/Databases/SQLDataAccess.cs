d5b6df625786e03c19ca9acc861b1c8d2d73b6c5
Ahsan Ali Siddiqi <44543152+Ahsan70@users.noreply.github.com>
2021-04-24 06:05:17 +0500

    Adding Dapper

---
+++ HotalAppLibrary/Databases/SQLDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace HotalAppLibrary.Databases
+{
+   public class SQLDataAccess
+    {
+        private readonly IConfiguration _configuration;
+
+        public SQLDataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public List<T> LoadData<T,U>(string sqlStatement,
+                                     U parameters,
+                                     string connectionStringName)
+        {
+            string connectionString = _configuration.GetConnectionString(connectionStringName);
+            using (IDbConnection connection =new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement,parameters).ToList();
+                return rows;
+            }
+        }
+    }
+}
